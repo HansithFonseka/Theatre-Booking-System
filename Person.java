93a7f91da0059b3d89348dd77f909e41c862532a
Hansith <hansithlochana@gmail.com>
2024-09-20 07:41:39 +0530

    Create Person class

---
+++ Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    private String name;
+    private String surname;
+    private String email;
+
+    public Person(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+    
