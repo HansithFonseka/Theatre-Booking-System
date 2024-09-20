81d3c0649c0131bda663b709d33527c4d04811a7
Hansith <hansithlochana@gmail.com>
2024-09-20 07:32:30 +0530

    Add methods

--- Ticket.java
+++ Ticket.java
@@ -10,3 +10,27 @@ public class Ticket {
         this.price = price;
         this.person = person;
     }
+
+    public void print() {
+        System.out.println("Person name: " + person.getName());
+        System.out.println("Person surname: " + person.getSurname());
+        System.out.println("Person email: " + person.getEmail());
+        System.out.println("Row: " + row);
+        System.out.println("Seat: " + seat);
+        System.out.println("Price: " + price);
+    }
+    public int getRow() {return row;
+    }
+    public void setRow(int row) {this.row = row;
+    }
+    public int getSeat() {return seat;
+    }
+    public void setSeat(int seat) {this.seat = seat;
+    }
+
+    public double getPrice() {return price;
+    }
+    public void setPrice(double price) {this.price = price;
+    }
+
+}
