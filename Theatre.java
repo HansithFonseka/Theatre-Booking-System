ecbefa479fb5117c529f3dfd0dfe0179a0813758
Hansith <hansithlochana@gmail.com>
2024-09-21 09:51:03 +0530

    Create Theatre class

---
+++ Theatre.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+import java.io.*;
+public class Theatre {
+    static Scanner optionIn = new Scanner(System.in);
+    static int[] row1= new int[12];
+    static int[] row2= new int[16];
+    static int[] row3= new int[20];
+    static ArrayList <Ticket> buyers = new ArrayList<>();   //array list to save all the tickets
